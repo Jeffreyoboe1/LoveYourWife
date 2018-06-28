b31cd4c1017c38d067b1119f5e80e00e4f0ad948
Jeff Padgett <jeff_padgett@hotmail.com>
2018-06-28 15:52:30 -0500

    Colors and Button Added. Added strings for day texts. Beginning to work on layout.

--- app/src/main/java/jeffpadgett/myapplication/ChallengeComplete.java
+++ app/src/main/java/jeffpadgett/myapplication/ChallengeComplete.java
@@ -29,14 +29,7 @@ public class ChallengeComplete extends AppCompatActivity {
         tvChallengeComplete.append("" + dayCompleted);
 
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
--- app/src/main/java/jeffpadgett/myapplication/MainActivity.java
+++ app/src/main/java/jeffpadgett/myapplication/MainActivity.java
@@ -23,14 +23,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
@@ -72,16 +64,6 @@ public class MainActivity extends AppCompatActivity {
             Log.d("INTTEST", ""+ nextDay);
             mViewPager.setCurrentItem(nextDay);  // do not need + 1, because pager starts at 0.
         }
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     }
 
