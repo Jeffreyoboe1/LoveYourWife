59a459fdd660ad42944af5e5d1d6fbb22fec8ecc
Jeff Padgett <jeff_padgett@hotmail.com>
2018-06-30 21:05:25 -0500

    Added next button to challenge completed, added more launcher icon?

--- app/src/main/java/jeffpadgett/myapplication/ChallengeComplete.java
+++ app/src/main/java/jeffpadgett/myapplication/ChallengeComplete.java
@@ -7,10 +7,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,12 +31,14 @@ public class ChallengeComplete extends AppCompatActivity {
     ImageView imgTrophy;
     ImageView imgRings;
     ImageView imgRings2;
+    Button btnNext;
     int dayCompleted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_complete);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -44,7 +49,7 @@ public class ChallengeComplete extends AppCompatActivity {
         tvChallengeComplete = findViewById(R.id.tvCompletedDay);
         tvInspiration = findViewById(R.id.tvCompletedInspiration);
         tvCongratulations = findViewById(R.id.tvCongrats);
-        ViewGroup transitionContainer = findViewById(R.id.transitionCompletedContainer);
+        btnNext = findViewById(R.id.btnNext);
 
         //set the completion day
         dayCompleted = getIntent().getIntExtra("DAY", 1);
@@ -61,15 +66,64 @@ public class ChallengeComplete extends AppCompatActivity {
 
         }
 
-        TextViewCompat.setAutoSizeTextTypeWithDefaults(tvCongratulations, TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backIntent = new Intent(ChallengeComplete.this, MainActivity.class);
+                backIntent.putExtra("NEXTDAY", dayCompleted);
+                startActivity(backIntent);
+
+
+            }
+        });
+
+
 
         ScaleAnimation anim2 = new ScaleAnimation(0f, 1f, 0f,1f,
                 Animation.RELATIVE_TO_SELF, .5f, Animation.RELATIVE_TO_SELF, .5f);
         anim2.setFillAfter(true);
         anim2.setDuration(2000);
+        anim2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                btnNext.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
         imgTrophy.startAnimation(anim2);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
