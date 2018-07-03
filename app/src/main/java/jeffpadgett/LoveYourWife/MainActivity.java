3487985ffb0ee6639ec406f767a3e872f60d1b6d
Jeff Padgett <jeff_padgett@hotmail.com>
2018-07-02 19:28:02 -0500

    Using Dynamic Links to send/share the app. Upgraded Gradle added firebase = dynamic links

--- app/src/main/java/jeffpadgett/LoveYourWife/MainActivity.java
+++ app/src/main/java/jeffpadgett/LoveYourWife/MainActivity.java
@@ -1,6 +1,9 @@
 package jeffpadgett.LoveYourWife;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.app.Fragment;
@@ -11,8 +14,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ToxicBakery.viewpager.transforms.ForegroundToBackgroundTransformer;
+import com.google.android.gms.appinvite.AppInviteInvitation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.dynamiclinks.DynamicLink;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.ShortDynamicLink;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,13 +32,18 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    String TAG;
+    int REQUEST_INVITE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Log.d("ParameterPassed", "onCreate in MainActivity called.");
+        TAG = "MainActivity";
+        REQUEST_INVITE = 13254;
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -66,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -79,6 +95,12 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+
+        if (id == R.id.action_share_app) {
+            onInviteClicked();
+
+
         }
 
         return super.onOptionsItemSelected(item);
@@ -218,4 +240,57 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
+
+    private void onInviteClicked() {
+
+
+
+        Task<ShortDynamicLink> shortTask = FirebaseDynamicLinks.getInstance().createDynamicLink()
+                .setLink(Uri.parse("https://www.google.com/")) // put website here
+                .setDynamicLinkDomain("loveyourwife.page.link")
+                // Open links with this app on Android
+                .setAndroidParameters(new DynamicLink.AndroidParameters.Builder().build())
+                .buildShortDynamicLink().addOnCompleteListener(new OnCompleteListener<ShortDynamicLink>() {
+                    @Override
+                    public void onComplete(@NonNull Task<ShortDynamicLink> task) {
+                        if (task.isSuccessful()) {
+                            // Short link created
+                            Uri shortLink = task.getResult().getShortLink();
+                            Uri flowchartLink = task.getResult().getPreviewLink();
+
+                            String dynamicString = shortLink.toString();
+
+
+                            String msg = "Hi, I have been using this app called LoveYourWife.  It can help improve your marriage. " + dynamicString;
+                            String subject = "Love Your Wife";
+                            String msgHtml = String.format("<p>Hi!  I've been using this app called Love Your Wife, and wanted to recommend it to you.  "
+                                    + "It has great ideas to improve your marriage and love your wife better. " +
+                                    " Click on this link to check it out. " + "<a href=\"%s\">Love Your Wife</a>!</p>", dynamicString);
+
+                            Intent sendIntent = new Intent();
+                            sendIntent.setAction(Intent.ACTION_SEND);
+                            sendIntent.putExtra(Intent.EXTRA_TEXT, msg);
+                            sendIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                            sendIntent.putExtra(Intent.EXTRA_HTML_TEXT, msgHtml);
+                            sendIntent.setType("text/plain");
+                            startActivity(Intent.createChooser(sendIntent, "Recommend Love Your Wife to a friend"));
+
+                        } else {
+                            // Error
+                            // ...
+                            Toast.makeText(MainActivity.this, "error " + task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
+
+
+
+
+
+
+
+    }
+
 }
